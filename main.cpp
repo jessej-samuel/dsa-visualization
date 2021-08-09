3438fd572afd0de1ee5ae0cb20202db75cb2e125
jessej-samuel <1234jessej@gmail.com>
2021-08-09 22:31:37 +0530

    Setup the grid and centered it.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 
 int main()
 {
-    float cell_size = 5.0;
-    sf::RenderWindow window(sf::VideoMode(530, 530), "MS Paint v8", sf::Style::Close | sf::Style::Titlebar);
+    float cell_size = 7.0;
+    int cell_count = 60;
+    int grid_length = cell_size * cell_count + cell_count + 2;
+    sf::RectangleShape grid(sf::Vector2f(grid_length, grid_length)); // Set the bg of the cells to white
+    sf::RenderWindow window(sf::VideoMode(706, 600), "Conway's Game of Life", sf::Style::Close | sf::Style::Titlebar);
     sf::RectangleShape cell(sf::Vector2f(cell_size, cell_size));
     // cell.setOrigin(cell_size / 2, cell_size / 2);
-    cell.setFillColor(sf::Color::Blue);
+    cell.setFillColor(sf::Color::Black);
+    grid.setFillColor(sf::Color::White);
 
     while (window.isOpen())
     {
@@ -24,33 +28,19 @@ int main()
             }
         }
 
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-        {
-            sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
-            cell.setPosition(static_cast<float>(mouse_position.x), static_cast<float>(mouse_position.y));
-        }
-
         window.clear();
+        float grid_x_offset = (window.getSize().x - grid_length) / 2;
+        float grid_y_offset = 20.0f;
+        grid.setPosition(grid_x_offset, grid_y_offset);
+        window.draw(grid);
+
         // window.setFramerateLimit(120);
         window.setFramerateLimit(0);
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < cell_count; x++)
         {
-            for (int y = 0; y < 100; y++)
+            for (int y = 0; y < cell_count; y++)
             {
-                cell.setPosition(sf::Vector2f((x + 3) * cell_size, (y + 3) * cell_size));
-                if ((x + y) % 2 == 0)
-                {
-                    cell.setFillColor(sf::Color::Red);
-                }
-                else if ((x + y) % 2 == 1)
-                {
-                    cell.setFillColor(sf::Color::Green);
-                }
-                else
-                {
-                    std::cout << "Err!";
-                }
-
+                cell.setPosition(sf::Vector2f((x)*cell_size + x + 2 + grid_x_offset, y + 2 + (y)*cell_size + grid_y_offset));
                 window.draw(cell);
             }
         }
